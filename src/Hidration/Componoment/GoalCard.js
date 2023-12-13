0473a2c255427c98379c97de2c4bd64723b12b87
Gcalcada <goncalocalcada7@gmail.com>
2023-12-13 00:13:01 +0000

    First

--- src/Hidration/Componoment/GoalCard.js
+++ src/Hidration/Componoment/GoalCard.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import {
   View,
   Text,
@@ -12,6 +12,7 @@ import {
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { format } from 'date-fns';
 import * as Notifications from 'expo-notifications';
+import { GiftedChat } from 'react-native-gifted-chat';
 
 
 
@@ -20,9 +21,14 @@ const GoalCard = ({ deviceName }) => {
   const [isModalVisible, setIsModalVisible] = useState(false);
   const [goal, setGoal] = useState('');
   const [reminderTime, setReminderTime] = useState(new Date());
-  const [showDatePicker, setShowDatePicker] = useState(false);
   const [currentTime, setCurrentTime] = useState(new Date());
   const [goals, setGoals] = useState([]);
+  const [isChatbotModalVisible, setIsChatbotModalVisible] = useState(false);
+  const [messages, setMessages] = useState([]);
+  const [messageIdCounter, setMessageIdCounter] = useState(1);
+  const [hasBotSaidHello, setHasBotSaidHello] = useState(false);
+
+
 
   const toggleModal = () => {
     setIsModalVisible(!isModalVisible);
@@ -30,67 +36,131 @@ const GoalCard = ({ deviceName }) => {
 
 
   const handleAddGoal = async () => {
-    const newGoal = { id: goals.length + 1, goal: goal, time: currentTime };
-
-    setGoals((prevGoals) => {
-      return [...prevGoals, newGoal];
-    });
-
-    setGoal('');
-    toggleModal();
+    const newGoal = { id: new Date().getTime(), goal: goal, time: currentTime };
 
     try {
-      const formattedDate = format(new Date(currentTime), 'yyyy-MM-dd');
-      const formattedTime = format(new Date(currentTime), 'HH:mm');
+      const formattedDate = format(new Date(currentTime), 'yyyy-MM-dd HH:mm:ss');
+      const updatedGoal = { ...newGoal, time: formattedDate };
 
-      const updatedGoal = { ...newGoal, time: `${formattedDate} ${formattedTime}` };
-
-      // Use a unique key for each goal based on its id
+      // Save the goal
       await AsyncStorage.setItem(`@goal:${newGoal.id}`, JSON.stringify(updatedGoal));
+
+      // Update the goals state
+      setGoals((prevGoals) => [...prevGoals, updatedGoal]);
     } catch (error) {
       console.log('Error saving data:', error);
+    }
+
+    toggleModal();
+  };
+  const handleGoalQuestion = async () => {
+    try {
+      const keys = await AsyncStorage.getAllKeys();
+      const goalKeys = keys.filter((key) => key.startsWith('@goal:'));
+
+      console.log(goalKeys);
+
+      if (goalKeys.length > 0) {
+        const lastGoalKey = goalKeys[goalKeys.length - 1];
+        const lastGoalString = await AsyncStorage.getItem(lastGoalKey);
+        const lastGoal = JSON.parse(lastGoalString); // Parse the correct string
+
+        const userQuestion = {
+          _id: new Date().getTime(),
+          text: 'What was my last goal?',
+          createdAt: new Date(),
+          user: {
+            _id: messageIdCounter,
+            name: 'User',
+          },
+        };
+
+        const chatbotResponse = {
+          _id: new Date().getTime() + 1,
+          text: `Your last goal was: ${lastGoal.goal}`,
+          createdAt: new Date(),
+          user: {
+            _id: 1,
+            name: 'ChatBot',
+          },
+        };
+
+        setMessages((prevMessages) => [...prevMessages, userQuestion, chatbotResponse]);
+      } else {
+        const chatbotResponse = {
+          _id: new Date().getTime(),
+          text: 'You don\'t have any goals yet. Set a goal using the "Add Your Goal" button.',
+          createdAt: new Date(),
+          user: {
+            name: 'ChatBot',
+          },
+        };
+
+        setMessages((prevMessages) => [...prevMessages, chatbotResponse]);
+      }
+    } catch (error) {
+      console.error('Error handling goal question:', error);
     }
   };
 
   const handleRemindButton = () => {
+    const notificationContent = {
+      title: 'Reminder',
+      body: 'It\'s time to update your goal!',
+    };
+
+    const notificationTrigger = {
+      seconds: 0,
+    };
+
     Notifications.scheduleNotificationAsync({
-      content: {
-        title: 'Reminder',
-        body: 'It\'s time to update your goal!',
-      },
-      trigger: {
-        seconds: 0,
-      },
-      channelId: 'GoalReminder', // Make sure this matches the channel ID used when setting the channel
+      content: notificationContent,
+      trigger: notificationTrigger,
+      channelId: 'GoalReminder',
     });
   };
 
+  const getData = async () => {
+    try {
+      const keys = await AsyncStorage.getAllKeys();
+      const goalKeys = keys.filter((key) => key.startsWith('@goal:'));
+
+      if (goalKeys.length > 0) {
+        // Sort the keys by date in descending order
+        const sortedKeys = goalKeys.sort((a, b) => {
+          const dateA = parseInt(a.split(':')[2]);
+          const dateB = parseInt(b.split(':')[2]);
+          return dateB - dateA;
+        });
+
+        // Retrieve and set all goals in sorted order
+        const allGoals = await Promise.all(
+          sortedKeys.map(async (key) => {
+            const goalString = await AsyncStorage.getItem(key);
+            return JSON.parse(goalString);
+          })
+        );
+
+        setGoals(allGoals);
+        console.log(allGoals);
+      } else {
+        setGoals([]); // No goals found
+      }
+    } catch (error) {
+      console.log('Error retrieving data:', error);
+    }
+  };
 
   useEffect(() => {
 
 
+    getData();
     const notificationListener = Notifications.addNotificationResponseReceivedListener(response => {
       const notificationData = response.notification.request.content.data;
       const actualDeviceName = notificationData.deviceName;
       console.log('Actual Device Name:', actualDeviceName);
     });
-    const getData = async () => {
-      try {
-        // Retrieve all keys that match the pattern '@goal:*'
-        const keys = await AsyncStorage.getAllKeys();
-        const goalKeys = keys.filter((key) => key.startsWith('@goal:'));
 
-        // Retrieve data for each goal key
-        const values = await AsyncStorage.multiGet(goalKeys);
-        const parsedGoals = values.map(([key, value]) => JSON.parse(value));
-
-        setGoals(parsedGoals);
-      } catch (error) {
-        console.log('Error retrieving data:', error);
-      }
-    };
-
-    getData();
 
 
     const interval = setInterval(() => {
@@ -102,6 +172,73 @@ const GoalCard = ({ deviceName }) => {
 
     }
   }, [reminderTime]);
+  const onSend = useCallback((messages = []) => {
+    setMessages((prevMessages) =>
+      GiftedChat.append(
+        prevMessages,
+        messages.map((message) => ({
+          ...message,
+          _id: `${messageIdCounter}-${new Date().getTime()}`
+        }))
+      )
+    );
+    setMessageIdCounter((prevCounter) => prevCounter + 1);
+  }, []);
+
+  const renderChatbotModalContent = () => (
+    <View style={styles.chatbotModalContent}>
+      <TouchableOpacity style={styles.closeButton} onPress={closeChatbotModal}>
+        <Text style={styles.closeButtonText}>X</Text>
+      </TouchableOpacity>
+      <Text style={styles.modalTitle}>Chat with the Bot:</Text>
+      <View style={styles.chatbotMessagesContainer}>
+        <GiftedChat
+          messages={messages}
+          onSend={(messages) => onSend(messages)}
+          user={{ _id: 1 }}
+          typingIndicatorTextStyle={{ color: '#888', fontSize: 12 }}
+          renderChatFooter={() => (
+            <Text style={{ alignSelf: 'center', color: '#888', fontSize: 12 }}>
+              ChatBot is typing...
+            </Text>
+          )}
+        />
+      </View>
+      <Button title="Ask about Goal" onPress={handleGoalQuestion} />
+    </View>
+  );
+
+
+  const toggleChatbotModal = () => {
+    setIsChatbotModalVisible(true);
+
+    if (!hasBotSaidHello) {
+      const responseDelay = 5000;
+      chatbotResponseTimer = setTimeout(() => {
+        const chatbotResponse = {
+          _id: new Date().getTime(),
+          text: 'Im Your virtual assistant how could i help you?',
+          createdAt: new Date(),
+          user: {
+            _id: messageIdCounter,
+            name: 'ChatBot',
+          },
+        };
+        setMessages((prevMessages) => [...prevMessages, chatbotResponse]);
+        setMessageIdCounter((prevCounter) => prevCounter + 1);
+
+        setHasBotSaidHello(true);
+      }, responseDelay);
+    }
+  };
+
+
+
+  const closeChatbotModal = () => {
+    setIsChatbotModalVisible(false);
+  };
+
+
 
   const renderModalContent = () => (
     <View style={styles.modalContent}>
@@ -127,29 +264,102 @@ const GoalCard = ({ deviceName }) => {
 
 
   return (
-    <View style={styles.card}>
-      <Text style={styles.title}>{deviceName ? `Good Morning, ${deviceName}` : 'Good Morning'}</Text>
-      <View style={styles.cardBody}>
-        <Text style={styles.cardTitle}>Your Goal</Text>
-        {goals.length > 0 && (
-          <View>
-            <Text style={styles.cardText}>Goal: {goals[goals.length - 1].goal}</Text>
-            <Text style={styles.cardText}>Saved Hour: {goals[goals.length - 1].time.toLocaleString()}</Text>
+    <View>
+      <View style={styles.card}>
+        <Text style={styles.title}>{deviceName ? `Hello Special User, ${deviceName}` : 'Good Morning'}</Text>
+        <View style={styles.cardBody}>
+          <Text style={styles.cardTitle}>Your Goal</Text>
+          {goals.length > 0 && (
+            <View>
+              <Text style={styles.cardText}>Goal: {goals[goals.length - 1].goal}</Text>
+              <Text style={styles.cardText}>Saved Hour: {goals[goals.length - 1].time.toLocaleString()}</Text>
+            </View>
+          )}
+        </View>
+        <View style={styles.cardFooter}>
+          <Button title="Add Your Goal" onPress={toggleModal} />
+          <View style={styles.buttonContainer}>
+            <Button title="Remind Me" onPress={handleRemindButton} />
           </View>
-        )}
+        </View>
+        <Modal animationType="fade" transparent={true} visible={isModalVisible}>
+          {renderModalContent()}
+        </Modal>
       </View>
-      <View style={styles.cardFooter}>
-        <Button title="Add Your Goal" onPress={toggleModal} />
-        <Button title="Remind Me" onPress={handleRemindButton} />
+
+
+      <View style={styles.container}>
+        <View style={styles.cardContainer}>
+
+        </View>
+
+        <TouchableOpacity style={styles.fixedButton} onPress={toggleChatbotModal}>
+          <Text style={styles.fixedButtonText}>Chat</Text>
+        </TouchableOpacity>
+
+        <Modal animationType="fade" transparent={true} visible={isChatbotModalVisible}>
+          {renderChatbotModalContent()}
+        </Modal>
       </View>
-      <Modal animationType="fade" transparent={true} visible={isModalVisible}>
-        {renderModalContent()}
-      </Modal>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  buttonContainer: {
+    marginHorizontal: 10,
+    flex: 1,
+    borderRadius: 8,
+  },
+  cardFooter: {
+    borderRadius: 8,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginTop: 10,
+  },
+  chatbotModalContent: {
+    backgroundColor: '#fff',
+    padding: 20,
+    borderRadius: 10,
+    width: '100%',
+    maxWidth: 500,
+    alignSelf: 'center',
+    flex: 1, // Ensure that the modal content takes the full height
+  },
+  chatbotMessagesContainer: {
+    flex: 1,
+    marginBottom: 20,
+  },
+
+
+  modalContent: {
+    backgroundColor: '#fff',
+    padding: 20,
+    borderRadius: 10,
+    width: '100%',
+    maxWidth: 500,
+    alignSelf: 'center',
+  },
+  fixedButton: {
+    position: 'absolute',
+    bottom: -300,
+    right: 16,
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+    backgroundColor: '#007bff',
+    justifyContent: 'center',
+    alignItems: 'center',
+    elevation: 5,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.3,
+    shadowRadius: 2,
+  },
+  fixedButtonText: {
+    color: '#fff',
+    fontSize: 18,
+  },
 
   modalContent: {
     backgroundColor: '#fff',
@@ -217,7 +427,7 @@ const styles = StyleSheet.create({
     fontSize: 16,
     marginBottom: 4,
   },
-  cardFooter: {},
+
 });
 
 export default GoalCard;
